fa90cc055e9a561d8023117f86c2c464d7470355
KY <kuoyehs@gmail.com>
2020-03-21 03:15:37 +0800

    build

---
+++ build/precache-manifest.991550ca4b3d9d3d4b4dfc643840035e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd8a989d1c0ddd8244306fd4cdaf68cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "293b0a4d5a8cbdde406b",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "84c40de82999dfc4b56c",
+    "url": "/static/css/main.829c451d.chunk.css"
+  },
+  {
+    "revision": "293b0a4d5a8cbdde406b",
+    "url": "/static/js/2.0d79dbad.chunk.js"
+  },
+  {
+    "revision": "84c40de82999dfc4b56c",
+    "url": "/static/js/main.4509e7f0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
